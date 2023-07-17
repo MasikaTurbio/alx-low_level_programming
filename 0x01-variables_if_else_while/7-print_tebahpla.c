f90f48bb8988d3c16e9e2d92f41f1ffa7cce2b60
MasikaTurbio <masikaturbio@gmail.com>
2023-07-17 11:35:02 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low <= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+}
