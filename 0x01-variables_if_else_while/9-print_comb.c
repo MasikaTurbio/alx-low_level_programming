f37093c1b101c623265671fc4e78ed3bb5d7fc50
MasikaTurbio <masikaturbio@gmail.com>
2023-07-17 12:11:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Returns: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(d)
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
